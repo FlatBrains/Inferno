c123ddcf41cc32951fc744076aeace8bfc068b1b
FlamingFire0 <p.manu2072@gmail.com>
2024-09-08 00:00:54 +0200

    testing (still)

--- zip.ps1
+++ zip.ps1
@@ -9,4 +9,9 @@ $message = Read-Host "Enter Git Commit Title"
 git commit -m $message
 git push -u origin main
 
-(Get-FileHash -a SHA1 pack.zip).Hash.ToLower()
+$hash = (Get-FileHash -a SHA1 pack.zip).Hash.ToLower()
+
+echo.
+echo "pack.zip's sha1 hash:"
+echo $hash
+$hash | clip
