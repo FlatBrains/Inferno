44b116a76b3b849121cdd8b0754e9ec51af06aaa
FlamingFire0 <p.manu2072@gmail.com>
2024-09-07 21:42:43 +0200

    updated zip.ps1 and made tungsten drill T3 golden

--- zip.ps1
+++ zip.ps1
@@ -1,3 +1,10 @@
+cd $PSScriptRoot
+
 rm pack.zip
 Compress-Archive assets, pack.mcmeta, pack.png pack.zip
+
+git add .
+git commit #exit by pressing esc and then :wq!
+git push -u origin main
+
 Get-FileHash -a SHA1 pack.zip
