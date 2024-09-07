023916939f3a4333a27c1abeb36b1379299b9a47
FlamingFire0 <p.manu2072@gmail.com>
2024-09-07 22:29:17 +0200

    testing

--- zip.ps1
+++ zip.ps1
@@ -5,8 +5,8 @@ Compress-Archive assets, pack.mcmeta, pack.png pack.zip
 
 git add .
 git status -s
-$message = Read-Host "Enter Git Commit Title: "
+$message = Read-Host "Enter Git Commit Title"
 git commit -m $message
 git push -u origin main
 
-Get-FileHash -a SHA1 pack.zip
+(Get-FileHash -a SHA1 pack.zip).Hash.ToLower()
