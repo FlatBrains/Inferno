669e501cb27c4aa8ac59e26f3fb80ce3e61321f3
FlamingFire0 <p.manu2072@gmail.com>
2024-09-15 14:56:09 +0200

    using 7z now

--- zip.ps1
+++ zip.ps1
@@ -1,13 +1,15 @@
 cd $PSScriptRoot
 
 rm pack.zip
-Compress-Archive assets, pack.mcmeta, pack.png pack.zip -CompressionLevel Fastest
+#Compress-Archive assets, pack.mcmeta, pack.png pack.zip -CompressionLevel Fastest
+
+7z a pack.zip assets pack.png pack.mcmeta
 
 git add .
 git status -s
 $message = Read-Host "Enter Git Commit Title"
 git commit -m $message
-git push -u origin main
+git push -u origin main 
 
 $hash = (Get-FileHash -a SHA1 pack.zip).Hash.ToLower()
 
